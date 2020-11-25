87be5121784a46c20120a3e4447e9e13ce81f280
Яна Антонюк <anaantonuk@MacBook-Air-Ana.local>
2020-11-26 00:21:38 +0300

    changed main

--- src/main/java/com/java/automation/lab/fall/antonyuk/core22/runner/Main.java
+++ src/main/java/com/java/automation/lab/fall/antonyuk/core22/runner/Main.java
@@ -1,5 +1,6 @@
 package com.java.automation.lab.fall.antonyuk.core22.runner;
 
+import com.java.automation.lab.fall.antonyuk.core22.dao.baseDao.FileDAO;
 import com.java.automation.lab.fall.antonyuk.core22.dao.baseDao.MochaDao;
 import com.java.automation.lab.fall.antonyuk.core22.dao.baseDao.SqlDAO;
 import com.java.automation.lab.fall.antonyuk.core22.dao.daoModel.AddressDAO;
@@ -12,12 +13,13 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws InvalidNameException, NoSuchMethodException,
             IllegalAccessException, InstantiationException, SQLException, InvocationTargetException, ClassNotFoundException {
-//        AddressDAO dao = AddressDAO.getInstance(new FileDAO<Address>(), "addresses");
-//        dao.create(new Address("Minsk", "Minsk", "Odincova", 1));
-//        AddressDAO dao2 = AddressDAO.getInstance(new MochaDao<Address>(), "addresses");
-//        dao2.create(new Address("Aa", "Vv", "Cc", 101));
-//        System.out.println(dao2.get(0).getNumber());
-//        Address address = new Address();
-//        address.setId(3);
+        AddressDAO dao = AddressDAO.getInstance(new FileDAO<Address>(), "addresses");
+        dao.create(new Address("Minsk", "Minsk", "Odincova", 1));
+        AddressDAO dao2 = AddressDAO.getInstance(new MochaDao<Address>(), "addresses");
+        dao2.create(new Address("Aa", "Vv", "Cc", 101));
+        System.out.println(dao2.get(0).getNumber());
+        SqlDAO<Address> sqlDAO = new SqlDAO<Address>();
+        sqlDAO.connect();
+
     }
 }
